d3b23200dfaf5b6950567b57a1b7f90ebc800195
김기서 <5rne@naver.com>
2023-01-09 21:27:37 +0900

    feat: Controller layer(not finished)

---
+++ src/main/java/com/example/urlshortener/controller/ApiController.java
@@ -0,0 +1,35 @@
+package com.example.urlshortener.controller;
+
+import com.example.urlshortener.dto.ShortUrlRequest;
+import com.example.urlshortener.dto.ShortUrlResponse;
+import com.example.urlshortener.service.ShortUrlService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("api/v1/urls")
+public class ApiController {
+    private final ShortUrlService urlService;
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ShortUrlResponse getShortUrl(@RequestBody @Valid ShortUrlRequest urlRequest) {
+        ShortUrlResponse shortUrlResponse = urlService.generateShortUrl(urlRequest);
+
+        return shortUrlResponse;
+    }
+
+    @GetMapping("/{shortUrl}")
+    public void redirectUrl(@PathVariable String shortUrl, HttpServletResponse response) throws IOException {
+        String originalUrl = urlService.decodeUrl(shortUrl);
+        response.sendRedirect(originalUrl);
+    }
+}
