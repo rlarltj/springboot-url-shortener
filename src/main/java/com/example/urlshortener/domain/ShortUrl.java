e33a2ecdb712d9e19d945bef770539251e1f8d94
김기서 <5rne@naver.com>
2023-01-11 17:17:28 +0900

    chore: ShortUrl 검증 로직 추가

--- src/main/java/com/example/urlshortener/domain/ShortUrl.java
+++ src/main/java/com/example/urlshortener/domain/ShortUrl.java
@@ -2,13 +2,12 @@ package com.example.urlshortener.domain;
 
 
 import lombok.*;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ShortUrl {
     private String shortUrl;
@@ -19,5 +18,26 @@ public class ShortUrl {
 
     public void increaseCount() {
         this.requestCount ++;
+    }
+
+    @Builder
+    protected ShortUrl(String shortUrl, String algorithm, int requestCount) {
+        validateShortUrl(shortUrl);
+        validateAlgorithm(algorithm);
+        this.shortUrl = shortUrl;
+        this.algorithm = algorithm;
+        this.requestCount = requestCount;
+    }
+
+    private void validateShortUrl(String shortUrl) {
+        if (!StringUtils.hasText(shortUrl)) {
+            throw new IllegalArgumentException("shortURL은 null이 저장될 수 없습니다.");
+        }
+    }
+
+    private void validateAlgorithm(String algorithm) {
+        if (!StringUtils.hasText(algorithm)) {
+            throw new IllegalArgumentException("단축 알고리즘은 null이 저장될 수 없습니다.");
+        }
     }
 }
